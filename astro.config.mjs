8ac28f8a6e0a3803f012c78278a0cf3c6b6caab2
Andrew Mason <andrewmason@Andrews-MacBook-Pro.local>
2024-04-04 10:41:39 -0400

    Add home video load on click function for better speed, update play/pause events, convert avif to jpeg

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import netlify from "@astrojs/netlify";
-
 import mdx from "@astrojs/mdx";
 
+// https://astro.build/config
 export default defineConfig({
   output: "static",
   adapter: netlify(),
   integrations: [mdx()]
-} 
-);
+});
